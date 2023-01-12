7d3a32f9d43562a1861524919c3ae184d4f1a02e
Guro <70147494+gurokirta@users.noreply.github.com>
2023-01-12 22:30:54 +0400

    final version

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 var fullName = prompt("შეიყვანეთ თქვენი სახელი სრულიად");
-// document.write(fullName);
+
 document.getElementById("fullName").innerHTML=fullName;
 
 var firstNum = prompt("შეიყვანეთ პირველი რიცხვი");
 var secondNum = prompt("შეიყვანეთ მეორე რიცხვი");
 var action = prompt("აირჩიეთ შემდეგი მოქმედებები + , - , / , *");
-
-// document.write(Number(firstNum) + Number(secondNum));
-
+var result = firstNum + secondNum;
 
 
 // If-Else
 if (action ==="+") { 
-  document.getElementById("action").innerHTML=(Number(firstNum) + Number(secondNum));
+  document.getElementById("action").innerHTML=result;
 } else if (action ==="-") { 
-    document.getElementById("action").innerHTML=(Number(firstNum) - Number(secondNum));
+    document.getElementById("action").innerHTML=result;
 } else if (action ==="/") {
-    document.getElementById("action").innerHTML=(Number(firstNum) / Number(secondNum));
+    document.getElementById("action").innerHTML=result;
 } else if (action ==="*") {
-    document.getElementById("action").innerHTML=(Number(firstNum) * Number(secondNum));
+    document.getElementById("action").innerHTML=result;
+} else if (action=NaN) {
+    document.getElementById("action__error").innerHTML=("არასწორი მოქმედება");
+    console.log("არასწორი მოქმედება");
 }
 else {
     document.getElementById("action__error").innerHTML=("არასწორი მოქმედება");
